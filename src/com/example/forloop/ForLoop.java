20995603be8801a14ee2faa856641a38c02863f9
Sumon Ahmed <sumoncse15@gmail.com>
2020-08-28 18:13:30 +0600

    initial commit

---
+++ src/com/example/forloop/ForLoop.java
@@ -0,0 +1,11 @@
+package com.example.forloop;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+        for (int n = 1; n <= 25; n++) {
+
+            System.out.println(n);
+        }
+    }
+}
